904e7c2fdbd9601310528b6dc07857a8d28d61b7
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-08-01 16:29:54 +0100

    added edit feature

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const deleteTodoById = (id) => {
         .then(res => res.json())
         .then(response => response)
         .catch(e => e);
-        location.reload(true);
+    location.reload(true);
 }
 
 async function getAllTodos() {
@@ -27,11 +27,12 @@ async function getAllTodos() {
         return (newTodo.innerHTML = `<p class="no-todo">${response}</p>`);
     } else {
         response.todos.forEach(todo => {
-            newTodo.innerHTML += `<div class="todo-card" id=${todo.id} >
-         <span class='delete' onClick=deleteTodoById(${todo.id})>X</span>
+            newTodo.innerHTML += `<div  class="todo-card" id=${todo.id} >
+         <span class='delete'onClick=deleteTodoById(${todo.id})>X</span>
          <p class="todo-date">${todo.created_at}</p>
          <br>
         <p  class="todo-description" > ${todo.name}</p>
+        <button onClick=updateTodoById(${todo.id})>Edit todo</button>
         </div>`;
         });
     }
@@ -59,5 +60,47 @@ document.querySelector('#add-button').addEventListener('click', () => {
         .then(res => res.json())
         .then(response => response)
         .catch(e => e);
-        location.reload(true);
+    window.location = './index.html'
 })
+
+async function updateTodoById(id) {
+    const backToHomeButton = document.getElementById('new-todo');
+    backToHomeButton.addEventListener('click' , (e)=>{
+        window.location = './index.html'
+    })
+    document.getElementById('main-todo-input').style.display = 'none';
+    document.getElementById('edit').style.display = 'block';
+    const response = await fetch(`http://localhost:5000/api/v1/todos/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(response => response)
+        .catch(e => e);
+
+    let formerTodo = response.todo.name;
+    let formerTodoTime = response.todo.created_at;
+    let formerTodoDiv = document.getElementById('former-todo');
+    formerTodoDiv.innerHTML = `EDIT TODO : <b>"${formerTodo}"</b> ? <br> TIME CREATED : <u> ${formerTodoTime}</u>` ;
+
+    let editedTodoText = document.getElementById('edit-todo').value;
+    document.getElementById('save-edit').addEventListener('click', () => {
+        todoBody = {
+            name: editedTodoText
+        };
+        fetch(`http:localhost:5000/api/v1/todos/${id}`, {
+                method: "PUT",
+                body: JSON.stringify(todoBody),
+                headers: {
+                    "content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(response => response)
+            .catch(e => e);
+
+        window.location = './index.html'
+    })
+}
